98a5fcf6278e8c034f3dea0201fdcf6e5a07d73a
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2025-07-19 20:40:25 +0800

    tech: integrate Google Analytics tracking scripts in layout and configure environment variable (#6)

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    env: {
+        NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
+    },
 };
 
 export default nextConfig;
